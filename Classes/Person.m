7729eb4d2764accf1a224a537b13ee238a38b3e0
Jentle <juntaozhi@163.com>
2020-06-08 06:39:37 +0800

    初始化

---
+++ Classes/Person.m
@@ -0,0 +1,18 @@
+//
+//  Person.m
+//  TestSpecDemo
+//
+//  Created by Jentle on 2020/6/8.
+//  Copyright © 2020 Jentle. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
++ (void)myPerson {
+    
+    NSLog(@"%@",__func__);
+}
+
+@end
